61f568786218b4b58e96137f6e2d6be5222cbffa
Trungvo93 <thanhtrungvo1993@gmail.com>
2022-12-03 11:36:27 +0700

    up dashboard

--- src/components/Content/Dashboard.jsx
+++ src/components/Content/Dashboard.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import "./Dashboard.css";
+import { useNavigate } from "react-router-dom";
 import Chart from "react-apexcharts";
 const Dashboard = () => {
+  const nagivate = useNavigate();
   const booksList = useSelector((state) => state.books);
   const usersList = useSelector((state) => state.users);
   const adminList = usersList.filter((e) => e.role === "admin").length;
   const libraryList = usersList.filter((e) => e.role === "library").length;
   const studentList = usersList.filter((e) => e.role === "student").length;
   const borrowandreturnList = useSelector((state) => state.borrowandreturn);
-  const returnList = borrowandreturnList.filter(
+  const returnedList = borrowandreturnList.filter(
     (e) => e.dayReturned !== ""
   ).length;
+  const top6RecentUsers = [];
+  usersList.slice(-6).map((e) => {
+    top6RecentUsers.unshift(e);
+  });
+  //Apexcharts
   const [optionsUsers, setOptionsUsers] = useState({
     labels: ["Admin", "Library", "Student"],
     plotOptions: {
@@ -36,29 +44,9 @@ const Dashboard = () => {
     studentList,
   ]);
 
-  const [optionsBorrowReturn, setOptionsBorrowReturn] = useState({
-    labels: ["Borrow", "Returned"],
-    plotOptions: {
-      pie: {
-        donut: {
-          labels: {
-            show: true,
-            value: {
-              show: true,
-            },
-          },
-        },
-      },
-    },
-  });
-  const [seriesBorrowReturn, setSeriesBorrowReturn] = useState([
-    borrowandreturnList.length,
-    returnList,
-  ]);
-
   return (
     <div className="container">
-      <div className="d-flex justify-content-between">
+      {/* <div className="d-flex justify-content-between">
         <div className="w-50">
           <Chart
             options={optionsUsers}
@@ -67,13 +55,156 @@ const Dashboard = () => {
             width="100%"
           />
         </div>
-        <div className="w-50">
-          <Chart
-            options={optionsBorrowReturn}
-            series={seriesBorrowReturn}
-            type="donut"
-            width="100%"
-          />
+      </div> */}
+
+      <div className="row">
+        <div
+          className="col-3  "
+          onClick={() => {
+            nagivate("/home/users");
+          }}>
+          <div className="bg-info rounded p-0">
+            <div className="d-flex gap-5 justify-content-between align-items-center  p-3">
+              <div className=" text-white">
+                <h1>{usersList.length}</h1>
+                <span>Users</span>
+              </div>
+              <div className="icon text-white">
+                <i className="bi bi-people  "></i>
+              </div>
+            </div>
+            <div className="text-center text-white  p-1 get-info">
+              <span>More info</span>
+              <i className="bi bi-arrow-right-circle ps-2"></i>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-3  "
+          onClick={() => {
+            nagivate("/home/products");
+          }}>
+          <div className="bg-success rounded p-0">
+            <div className="d-flex gap-5 justify-content-between align-items-center  p-3">
+              <div className=" text-white">
+                <h1>{booksList.length}</h1>
+                <span>Books</span>
+              </div>
+              <div className="icon text-white">
+                <i className="bi bi-journals"></i>
+              </div>
+            </div>
+            <div className="text-center text-white  p-1 get-info">
+              <span>More info</span>
+              <i className="bi bi-arrow-right-circle ps-2"></i>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-3  "
+          onClick={() => {
+            nagivate("/home/borrowreturn");
+          }}>
+          <div className="bg-warning rounded p-0">
+            <div className="d-flex gap-5 justify-content-between align-items-center  p-3">
+              <div className=" text-white">
+                <h1>{borrowandreturnList.length}</h1>
+                <span>Borrows</span>
+              </div>
+              <div className="icon text-white">
+                <i className="bi bi-clipboard-minus"></i>
+              </div>
+            </div>
+            <div className="text-center text-white  p-1 get-info">
+              <span>More info</span>
+              <i className="bi bi-arrow-right-circle ps-2"></i>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-3  "
+          onClick={() => {
+            nagivate("/home/borrowreturn");
+          }}>
+          <div className="bg-danger rounded p-0">
+            <div className="d-flex gap-5 justify-content-between align-items-center  p-3">
+              <div className=" text-white">
+                <h1>{returnedList}</h1>
+                <span>Returned</span>
+              </div>
+              <div className="icon text-white">
+                <i class="bi bi-clipboard-check"></i>
+              </div>
+            </div>
+            <div className="text-center text-white  p-1 get-info">
+              <span>More info</span>
+              <i className="bi bi-arrow-right-circle ps-2"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Top 5 last member */}
+      <div className="row mt-5 ">
+        <div className="col-6 ">
+          <div className="border bg-black bg-opacity-50 shadow p-2 rounded text-white">
+            <div>
+              <span className="p-2 bg-danger d-inline-block rounded fw-bold">
+                Lastest Members
+              </span>
+            </div>
+            <div className="row mt-2">
+              <div className="col-4 text-center my-3">
+                <img
+                  src={top6RecentUsers[0].avatar}
+                  alt=""
+                  className="rounded-circle w-50"
+                />
+                <p className="text-capitalize">{top6RecentUsers[0].name}</p>
+              </div>
+              <div className="col-4 text-center my-3">
+                <img
+                  src={top6RecentUsers[1].avatar}
+                  alt=""
+                  className="rounded-circle w-50"
+                />
+                <p className="text-capitalize">{top6RecentUsers[1].name}</p>
+              </div>
+              <div className="col-4 text-center my-3">
+                <img
+                  src={top6RecentUsers[2].avatar}
+                  alt=""
+                  className="rounded-circle w-50"
+                />
+                <p className="text-capitalize">{top6RecentUsers[2].name}</p>
+              </div>
+
+              <div className="col-4 text-center my-3">
+                <img
+                  src={top6RecentUsers[3].avatar}
+                  alt=""
+                  className="rounded-circle w-50"
+                />
+                <p className="text-capitalize">{top6RecentUsers[3].name}</p>
+              </div>
+              <div className="col-4 text-center my-3">
+                <img
+                  src={top6RecentUsers[4].avatar}
+                  alt=""
+                  className="rounded-circle w-50"
+                />
+                <p className="text-capitalize">{top6RecentUsers[4].name}</p>
+              </div>
+              <div className="col-4 text-center my-3">
+                <img
+                  src={top6RecentUsers[5].avatar}
+                  alt=""
+                  className="rounded-circle w-50"
+                />
+                <p className="text-capitalize">{top6RecentUsers[5].name}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
